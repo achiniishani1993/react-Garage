d5e6a4e432ccec24c54abed7e0fc0789592a55b4
achiniishani1993 <achiniishani1993@gmail.com>
2026-01-05 11:25:57 +0100

    counter app exercise

---
+++ tutoral2/src/Componenet/Button.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const Button = ({ minValue = 0 }) => {
+  const [value, SetValue] = useState(minValue);
+
+  const handleIncrementCounter = () => {
+    SetValue((prevValue) => prevValue + 1);
+  };
+
+  const handleDecrementCounter = () => {
+    SetValue((prevValue) => prevValue - 1);
+  };
+  const handleReset = () => {
+    SetValue(minValue);
+  };
+
+  return (
+    <div className="d-flex align-items-center gap-2">
+      <button
+        className="btn btn-outline-primary"
+        style={{ width: "90px", marginTop: "20px" }}
+        onClick={handleIncrementCounter}
+      >
+        +
+      </button>
+      <span className="text-primary" style={{ marginTop: "20px" }}>
+        {value}
+      </span>
+      <button
+        className="btn btn-outline-primary"
+        style={{ width: "90px", marginTop: "20px" }}
+        onClick={handleDecrementCounter}
+      >
+        -
+      </button>
+      <button
+        className="btn btn-outline-primary"
+        style={{ width: "90px", marginTop: "20px" }}
+        onClick={handleReset}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default Button;
