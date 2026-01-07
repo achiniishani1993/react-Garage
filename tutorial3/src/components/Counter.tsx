78070a9cdaa8808935422687add17252dba09dee
achiniishani1993 <achiniishani1993@gmail.com>
2026-01-07 20:26:29 +0100

    count app with autorest- useeffect learning

---
+++ tutorial3/src/components/Counter.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState("counting...");
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  useEffect(() => {
+    
+    if (count >= 10 || count <= -10) {
+      setStatus("Limit reached! Resetting...");
+
+     const autoReset = setTimeout(() => {
+        setCount(0);
+        setStatus("counting...");
+      }, 2000);
+      return () => clearTimeout(autoReset);
+    }
+    
+  }, [count]);
+
+  return (
+    <div className="d-flex align-items-center gap-2">
+      <button
+        className="btn btn-outline-primary"
+        style={{ width: "90px", marginTop: "20px" }}
+        onClick={increment}
+      >
+        +
+      </button>
+      <span className="text-primary" style={{ marginTop: "20px" }}>
+        {count}
+      </span>
+      <button
+        className="btn btn-outline-primary"
+        style={{ width: "90px", marginTop: "20px" }}
+        onClick={decrement}
+      >
+        -
+      </button>
+      <span>{status}</span>
+    </div>
+  );
+};
+
+export default Counter;
